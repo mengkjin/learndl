84a9471fa48e607388fcccdecd331781809be808
mengkjin <mengkjin@outlook.com>
2024-07-19 21:24:38 +0800

    20240719-01

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,2 +1,2 @@
-from .nn import DataAPI , Predictor , ModelTestor , Trainer
+from .nn import DataAPI , Predictor , ModelTestor , Trainer , HiddenExtractor
 from .factor import FmpManager , PerfManager , fmp_test , perf_test
