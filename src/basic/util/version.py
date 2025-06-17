9827628d9ee94201e876e5da94b3d68ed075c5d9
mengkjin <mengkjin@outlook.com>
2025-06-17 00:33:29 -0400

    little changes to the version.py

--- src/basic/util/version.py
+++ src/basic/util/version.py
@@ -3,8 +3,13 @@
 import torch
 from pathlib import Path
 
-def torch_load(path : str | Path , weights_only : bool = False , **kwargs):
-    if torch.__version__ < '2.6.0':
-        return torch.load(path , **kwargs)
-    else:
-        return torch.load(path , weights_only = weights_only , **kwargs)
+def torch_load_old(path : str | Path , weights_only : bool = False , **kwargs):
+    return torch.load(path , **kwargs)
+    
+def torch_load_new(path : str | Path , weights_only : bool = False , **kwargs):
+    return torch.load(path , weights_only = weights_only , **kwargs)
+
+if torch.__version__ < '2.6.0':
+    torch_load = torch_load_old
+else:
+    torch_load = torch_load_new
