818b63cc4fed88a9bfb81acc07cdfefb0f28634a
mengkjin <mengkjin@outlook.com>
2025-12-26 15:51:00 -0500

    minor fix

--- src/scripts/0_check/2_clear_pull.py
+++ src/scripts/0_check/2_clear_pull.py
@@ -8,18 +8,28 @@
 # mode: shell
 
 import subprocess
+from pathlib import Path
 from src.proj import Logger
 from src.app import BackendTaskRecorder
 
 @BackendTaskRecorder()
 def main(**kwargs):
+    # clean local changes
     Logger.highlight("Clean local changes...")
     subprocess.run(['git', 'reset', '--hard', 'HEAD'], check=True)
     subprocess.run(['git', 'clean', '-fd'], check=True)
     
+    # pull latest code
     Logger.highlight("Pull latest code...")
     result = subprocess.run(['git', 'pull'], capture_output=True, text=True, check=True)
     Logger.success(f"Done: {result.stdout}")
+
+    # remove empty folders in src
+    Logger.highlight("Remove empty script folders in src...")
+    for folder in Path('src').rglob('*/'):
+        if folder.is_dir() and not list(folder.iterdir()):
+            Logger.success(f"Remove empty folder: {folder}")
+            folder.rmdir()
 
     return f'Finish pull: {result.stdout}'
 
