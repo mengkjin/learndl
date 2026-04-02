15e036ab9628a6a125c8328c458c661b8559d1ab
mengkjin <mengkjin@outlook.com>
2026-04-02 15:19:11 -0400

    minor fix

--- scripts/0_check/5_temporary_fix.py
+++ scripts/0_check/5_temporary_fix.py
@@ -11,12 +11,27 @@ from src.proj.util import ScriptTool
 from src.api import UpdateAPI
 from src.data.crawler.announcement.agent import AnnouncementAgent
 from src.data.util.classes import DataCache
+from src.proj import DB
+from src.res.factor.risk import TuShareCNE5_Calculator
 
 @ScriptTool('temporary_fix')
 def main(**kwargs):
-    DataCache.purge_all(confirm = True)
     # AnnouncementAgent.update()
-    UpdateAPI.rollback(rollback_date = 20260327)
+    calculator = TuShareCNE5_Calculator()
+    paths = DB.paths('models' , 'tushare_cne5_coef' , start = 0)
+    for path in paths:
+        df = DB.load_df(path)
+        if 'factor_name' in df.columns:
+            ...
+        if df.index.name == 'factor_name':
+            df = df.reset_index(drop = False)
+        elif df.index.name is None and df.index.get_level_values(None)[0] == 'market': # type: ignore
+            df.index.name = 'factor_name'
+            df = df.reset_index(drop = False)
+        else:
+            date = DB.path_date(path)
+            df = calculator.get_coef(date , read = False)
+        DB.save_df(df , path)
         
 if __name__ == '__main__':
     main()
